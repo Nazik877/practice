d2e15a8fb8149f7070bc39881210198a2581d54a
Nazik877 <mukatovanazik75@gmail.com>
2026-01-16 15:54:26 +0600

    useCounter

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Counter from './components/Counter';
 import './App.css';
 
 import { Routes, Route, Link } from 'react-router-dom';
+import Toggle from './components/Toggle';
 import BooleanToggle from './components/BooleanToggle'
 import ClickCounter from './components/ClickCounter';
 import Button from './components/Button'
@@ -32,13 +33,7 @@ function App() {
       
       <Routes>
    <Route path ='/counter' element={<Counter/>}/>
-   <Route path ='/boolean-toggle' element={<BooleanToggle/>}/>
-   <Route path = '/click-counter' element={<ClickCounter/>}/>
-   <Route path = '/button' element={<Button/>}/>
-   <Route path ='/color-switcher' element={<ColorSwitcher/>}/>
-  //  <Route path ='/even-odd-checker' element={<EvenOddChecker/>}/>
-   <Route path = '/hello' element={<Hello/>}/>
-   <Route path = '/name-switcher' element={<NameSwitcher/>}/>
+  <Route path ='/toggle' element={<Toggle/>}/>
 
 
   
@@ -65,66 +60,66 @@ export default App;
 
 
 
-// // import Hello from './components/Hello';
-// import UserCard from './components/UserCard';
-// import Litte from './components/Litte';
-// import Counter from './components/Counter';
-// import Toggle from './components/Toggle';
-// import Simple from './components/Simple';
-// import ToggleOnOff from './components/ToggleOnOff';
-// import TrimString from './components/TrimString';
-// import EvenOddChecker from './components/EvenOddChecker';
-// import ClickCounter from './components/ClickCounter';
-// import IncreaseFontSize from './components/IncreaseFontSize';
-// import NameSwitcher from './components/NameSwitcher';
-// import BooleanToggle from './components/BooleanToggle';
+// // // import Hello from './components/Hello';
+// // import UserCard from './components/UserCard';
+// // import Litte from './components/Litte';
+// // import Counter from './components/Counter';
+// // import Toggle from './components/Toggle';
+// // import Simple from './components/Simple';
+// // import ToggleOnOff from './components/ToggleOnOff';
+// // import TrimString from './components/TrimString';
+// // import EvenOddChecker from './components/EvenOddChecker';
+// // import ClickCounter from './components/ClickCounter';
+// // import IncreaseFontSize from './components/IncreaseFontSize';
+// // import NameSwitcher from './components/NameSwitcher';
+// // import BooleanToggle from './components/BooleanToggle';
 
 
 
 
-// import{useCounter} from './huks/useCounter';
-// import './App.css';
+// // import{useCounter} from './huks/useCounter';
+// // import './App.css';
 
 
-// function App(){
-//   const {count, increment, decrement, reset}= useCounter
-//   (10);
-//   return(
-//     <div className="App">
+// // function App(){
+// //   const {count, increment, decrement, reset}= useCounter
+// //   (10);
+// //   return(
+// //     <div className="App">
 
-//       <h1>{count}</h1>
-//       <button onClick={increment}>+</button>
-//       <button onClick={decrement}>-</button>
-//       <button onClick={reset}>reset </button>
-//     </div>
-//   )
-// }
-
-
-
+// //       <h1>{count}</h1>
+// //       <button onClick={increment}>+</button>
+// //       <button onClick={decrement}>-</button>
+// //       <button onClick={reset}>reset </button>
+// //     </div>
+// //   )
+// // }
 
 
 
 
 
 
-// function App() {
-//   return (
-//     <div className="App">
-      {/* <Hello/> */}
-      {/* <Button text="Нажми на меня" />
-      <UserCard
-      name="Ivan"/> */}
-      {/* <Counter/> */}
-      {/* <Toggle/> */}
-      {/* <Simple/> */}
-    {/* //  <ToggleOnOff/> */}
-      {/* <TrimString/> */}
-      {/* <EvenOddChecker/> */}
-      {/* <ClickCounter/> */}
-      {/* <IncreaseFontSize/> */}
-      {/* <NameSwitcher/> */}
-//       <BooleanToggle/>
+
+
+
+// // function App() {
+// //   return (
+// //     <div className="App">
+//       {/* <Hello/> */}
+//       {/* <Button text="Нажми на меня" />
+//       <UserCard
+//       name="Ivan"/> */}
+//       {/* <Counter/> */}
+//       // {/* <Toggle/> */}
+//       {/* <Simple/> */}
+//     {/* //  <ToggleOnOff/> */}
+//       {/* <TrimString/> */}
+//       {/* <EvenOddChecker/> */}
+//       {/* <ClickCounter/> */}
+//       {/* <IncreaseFontSize/> */}
+//       {/* <NameSwitcher/> */}
+// //       <BooleanToggle/>
  
 
      
--- src/huks/Application.jsx
+++ src/huks/Application.jsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import {useTimer} from '../huks/useTimer';
+// import React from 'react';
+// import {useTimer} from '../huks/useTimer';
 
-const Application = () => {
-    const timer = useTimer( )
-        return (
-            <div>
-                <h1>Current time:{timer}</h1>
-            </div>
-        );
+// const Application = () => {
+//     const timer = useTimer( )
+//         return (
+//             <div>
+//                 <h1>Current time:{timer}</h1>
+//             </div>
+//         );
    
-};
-export default Application;
+// };
+// export default Application;
